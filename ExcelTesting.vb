9c824e393405b46b43fac005e7e60d91706db21d
Leo-Zubiri <leo.zubiri@outlook.com>
2024-03-05 22:33:44 -0600

    Add [Excel Creation] Create an excel file with headers and content of the datatable into a specific cell

--- ExcelTesting.vb
+++ ExcelTesting.vb
@@ -1,4 +1,6 @@
-﻿Public NotInheritable Class ExcelTesting
+﻿Imports System.IO
+
+Public NotInheritable Class ExcelTesting
     Public Shared Function MyDT() As DataTable
 
         Dim table As DataTable = New DataTable()
@@ -21,4 +23,41 @@
 
         Return table
     End Function
+
+    Public Shared Sub CreateExcel(dtable As DataTable)
+        Try
+            Dim ExcelApp As Object
+            ExcelApp = CreateObject("Excel.Application")
+
+            Dim wb As Object
+            wb = ExcelApp.Workbooks.Add
+
+            Dim sheet As Object
+            sheet = wb.Worksheets(1)
+
+            Dim rowAdjust As Integer = 1 'Set content next to headers
+            Dim excelCell As String = "B2"
+
+            ' Set Headers
+            For col = 0 To dtable.Columns.Count - 1
+                sheet.Range(excelCell).Offset(0, col).Value = dtable.Columns.Item(col).ColumnName
+            Next
+
+            ' Set the content of the datatable
+            For row = 0 To dtable.Rows.Count - 1
+                For col = 0 To dtable.Columns.Count - 1
+                    'sheet.Cells(row + 1, col + 1).Value = dtable.Rows(row).Item(col)
+                    sheet.Range(excelCell).Offset(row + rowAdjust, col).Value = dtable.Rows(row).Item(col)
+                Next col
+            Next row
+
+            Dim ruta = My.Computer.FileSystem.SpecialDirectories.MyDocuments
+            wb.SaveAs(Path.Combine(ruta, "Test", "Excel.xlsx"))
+            wb.Close
+            ExcelApp.Quit()
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString())
+        End Try
+
+    End Sub
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,5 +1,6 @@
 ﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim dtable = ExcelTesting.MyDT()
+        ExcelTesting.CreateExcel(dtable)
     End Sub
 End Class
